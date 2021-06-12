6e10c5abf1d92c2e06decb3c3d124f902556dfd8
paulinacyran <paulinacyran@users.noreply.github.com>
2021-06-12 15:19:10 +0200

    dodanie klasy Student

--- zad.py
+++ zad.py
@@ -1,29 +1,33 @@
-lista_ocen = [2.0, 5.0, 3.0, 5.0]
+class Student():
+     def __init__(self, imie, nazwisko, kierunek):
+        self.imie =  imie
+        self.nazwisko = nazwisko
+        self.kierunek = kierunek
+        self.lista_ocen = [2.0, 5.0, 3.0, 5.0]
 
-# Zad 1
-def dodaj_ocene(ocena):
-    # Sprawdzam zakres ocen
-    if ocena >= 2.0 and ocena <= 6.0:
-        lista_ocen.append(ocena)
-        return True
-    else:
-        return False
+     def dodaj_ocene(self, ocena):
+         # Sprawdzam zakres ocen
+         if ocena >= 2.0 and ocena <= 6.0:
+             self.lista_ocen.append(ocena)
+             return True
+         else:
+             return False
 
-if dodaj_ocene(1.0):
-    print("Dodano ocenę")
-else:
-    print("Nie udało się dodać oceny")
+     # if dodaj_ocene(1.0):
+     #     print("Dodano ocenę")
+     # else:
+     #     print("Nie udało się dodać oceny")
 
-# Zad 2
-def wylicz_srednia():
-    print (sum(lista_ocen)/len(lista_ocen))
+     def wylicz_srednia(self):
+         print (sum(self.lista_ocen)/len(self.lista_ocen))
 
-wylicz_srednia()
+     def wylicz_srednia_n(self, n):
+         self.lista_ocen.reverse()
+         print (sum(self.lista_ocen[0:n])/len(self.lista_ocen[0:n]))
 
-# Zad 3
-def wylicz_srednia_n(n):
-    lista_ocen.reverse()
-    print (lista_ocen)
-    print (sum(lista_ocen[0:n])/len(lista_ocen[0:n]))
 
-wylicz_srednia_n(3)
+adam_nowak = Student("Adam", "Nowak", "tester")
+print(adam_nowak.imie)
+print(adam_nowak.lista_ocen)
+adam_nowak.wylicz_srednia()
+adam_nowak.wylicz_srednia_n(3)
